940fbdc02464b14fe1b1bcafbb1e405e19e0653e
Yincheng Ren <yr5ka@virginia.edu>
2020-06-16 21:26:32 -0400

    events

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-require('events');
+const EventEmitter = require('events');
+const { emitWarning } = require('process');
+const emitter = new EventEmitter();
+
+//Register a listener
+emitter.on('messageLogged',function(){console.log('listener called');
+});
+
+//Raise event, but need listener
+emitter.emit('messageLogged');
+
